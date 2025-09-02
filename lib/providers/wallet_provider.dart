0b7c1f01a59a1c176e82503ce2c43f13e5a2100e
D-Emman <102307238+D-Emman@users.noreply.github.com>
2025-09-02 14:33:07 +0100

    comments

--- lib/providers/wallet_provider.dart
+++ lib/providers/wallet_provider.dart
@@ -1,4 +1,4 @@
-// ===============================
+
 // lib/providers/wallet_provider.dart
 // ===============================
 import 'package:flutter/material.dart';
--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -1,5 +1,5 @@
 // lib/services/supabase_service.dart
-// ===============================
+
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Supa {
